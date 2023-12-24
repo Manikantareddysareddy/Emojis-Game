0ba116bd001b2c582464da9478d236e610818c1f
Manikantareddysareddy <149862326+Manikantareddysareddy@users.noreply.github.com>
2023-12-24 21:17:47 +0530

    Update index.js

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,1 +1,76 @@
-// Write your code here.
+
+import './index.css'
+
+import {NavBar, NavBar1} from '../NavBar'
+
+const WinCard = props => {
+  const {Score, newGameStart} = props
+  const newGame = () => {
+    newGameStart()
+  }
+  return (
+    <div className="container">
+      <NavBar1 />
+      <div className="top-bg-container1">
+        <div className="Won-container">
+          <div className="won-data-container">
+            <h1 className="won-heading">You Won</h1>
+            <div className="won-para-container">
+              <p className="won-para">Best Score</p>
+              <p className="won-para1">{Score + 1}/12</p>
+              <button
+                className="play-again-btn"
+                type="button"
+                onClick={newGame}
+              >
+                Play Again
+              </button>
+            </div>
+          </div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
+            className="won-image"
+            alt="win or lose"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const LoseCard = props => {
+  const {Score, newGameStart} = props
+  const newGame = () => {
+    newGameStart()
+  }
+  console.log(Score)
+  return (
+    <div className="container">
+      <NavBar1 />
+      <div className="top-bg-container1">
+        <div className="Won-container">
+          <div className="won-data-container">
+            <h1 className="won-heading">You Lose</h1>
+            <div className="won-para-container">
+              <p className="won-para">Score</p>
+              <p className="won-para1">{Score}/12</p>
+              <button
+                className="play-again-btn"
+                type="button"
+                onClick={newGame}
+              >
+                Play Again
+              </button>
+            </div>
+          </div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
+            className="won-image"
+            alt="win or lose"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+export {WinCard, LoseCard}
